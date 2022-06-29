ab564b1cc90e30acdbfe0fabd772e7949e1d8860
nishaslidetech <nisha.dhiman@slidetech.in>
2022-06-29 16:34:53 +0530

    test

--- src/test/java/stepDefination/TakeImageStep.java
+++ src/test/java/stepDefination/TakeImageStep.java
@@ -1,17 +1,22 @@
-package BaseClass;
+package stepDefination;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import BaseClass.SetUPClass;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 
 public class TakeImageStep extends SetUPClass {
 	@After
 	public void tearDown(Scenario scenario) {
-	
+
 		if (scenario.isFailed()) {
-			//String screenshotName = scenario.getName().replaceAll(" ", "_");
+			// String screenshotName = scenario.getName().replaceAll(" ", "_");
 			try {
 				File srcPath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 				byte[] newFile = FileUtils.readFileToByteArray(srcPath);
